489cc42a5d13f251a269f15bcb0c3cc865351031
Tameika Thompson <meekmeek92@MacBook-Pro-82.local>
2024-09-17 11:58:17 -0400

    added function main

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+function main(){
+const growMe = document.getElementById('grow-me');
+growMe.classList.add('big');
+
+const shrinkMe = document.getElementById('shrink-me')
+shrinkMe.classList.remove('big');
+
+
+
+
+}
+
+
+
+
+
+
