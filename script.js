efd73ad79580bd618b61046c6a4123a7ce9d3f55
Tameika Thompson <meekmeek92@MacBook-Pro-82.local>
2024-09-18 17:56:13 -0400

    added link correction and welcome message for when user en ters their name

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function main(){
 const growMe = document.getElementById('grow-me');
 growMe.classList.add('big');
 
-const shrinkMe = document.getElementById('shrink-me')
+const shrinkMe = document.getElementById('shrink-me');
 shrinkMe.classList.remove('big');
 
 const listItems = document.querySelectorAll('li');
@@ -15,11 +15,21 @@ const listItems = document.querySelectorAll('li');
 listItems.forEach((item) => {
     console.log(item.textContent);
 })
-const brokenLink = document.querySelector('broken-link');
-brokenLink.href = "https://www.example.com"
+const brokenLink = document.querySelector('.link'); // . link is the class name 
+brokenLink.href = "https://www.example.com";
+brokenLink.textContent = 'somewhere';
 
+const hideMe= document.getElementById('hide-me');
+   hideMe.style.display = 'none';
 
+  const showMe = document.getElementById('show-me');
+  showMe.style.display = 'block';
 
+  //const name = document.getElementById('name').value;
+ const usertName = document.getElementById('name').value;
+
+   
+   document.querySelector('h1').textContent = "Welcome " + usertName;
 
 }
 
