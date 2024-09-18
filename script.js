2167236194675095492a0f1fa15f09e8f0ac671a
Tameika Thompson <meekmeek92@MacBook-Pro-82.local>
2024-09-18 17:35:59 -0400

    added link logic and a foreach for listitems

--- script.js
+++ script.js
@@ -10,6 +10,14 @@ growMe.classList.add('big');
 const shrinkMe = document.getElementById('shrink-me')
 shrinkMe.classList.remove('big');
 
+const listItems = document.querySelectorAll('li');
+
+listItems.forEach((item) => {
+    console.log(item.textContent);
+})
+const brokenLink = document.querySelector('broken-link');
+brokenLink.href = "https://www.example.com"
+
 
 
 
